70ba6e3ae226cba175c727f699d2471da043ae02
Aaradhya Singh <aaradhyas438@gmail.com>
2023-11-13 11:42:14 +0530

    navbar added

---
+++ src/Components/Navbar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import '../css/Navbar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart, faBars, faClose } from '@fortawesome/free-solid-svg-icons';
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="container">
+        <h1 className="logotxt"><a href="#">ecom</a></h1>
+        <ul className="navlink">
+          <a href="#">Home</a>
+          <a href="#">Product</a>
+          <a href="#">About</a>
+          <a href="#">Contact</a>
+        </ul>
+        <div className="cartarea">
+          <button>Login</button>
+          <p><FontAwesomeIcon icon={faShoppingCart} />
+          </p>
+        <p className='menuBar'><FontAwesomeIcon icon={faBars}/></p>
+        <p className='closeMenuBar'><FontAwesomeIcon icon={faClose}/></p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Navbar;
